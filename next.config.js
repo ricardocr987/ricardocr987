7d5f8d3453b8f0210352c0ecee67e808c604fe9a
ricardocr987 <32191898+ricardocr987@users.noreply.github.com>
2022-05-21 18:27:07 +0200

    akamai

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
 };
 
 module.exports = nextConfig;
